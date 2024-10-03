eb31e7430a8d518fdb6b4b9bc694ef27e123f0cb
roma0000 <hishamtaha047@gmail.com>
2024-10-04 00:09:23 +0300

    added vercel.jsonn updated01

--- server/index.js
+++ server/index.js
@@ -10,13 +10,22 @@ const categoryRouter = require("./routes/categories.routes");
 const path = require("path");
 const bodyParser = require("body-parser");
 
+const allowedOrigins = [
+  "https://lms-mern-stack-client-gif3m4zun-hisham19119s-projects.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
+
 app.use(cors());
 dbConnection();
 app.use(express.json());
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api/users", userRouter);
