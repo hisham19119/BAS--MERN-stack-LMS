ba6458468b2d6f36b185357d551882ce4c3df550
roma0000 <hishamtaha047@gmail.com>
2024-10-03 18:13:52 +0300

    added vercel.jsonn updated 6

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const generateJWT = require("../utils/generate.jwt");
 const httpStatusText = require("../utils/httpStatusText");
 
