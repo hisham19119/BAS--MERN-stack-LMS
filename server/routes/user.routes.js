ab42a204a91340ba476efd0ea2c19d5b4d4fe409
roma0000 <hishamtaha047@gmail.com>
2024-10-04 00:18:47 +0300

    added vercel.jsonn updated02

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,28 @@ const router = express.Router();
 const userController = require("../controller/user.controller");
 const authController = require("../controller/auth.controller");
 
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://lms-mern-stack-client-gif3m4zun-hisham19119s-projects.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE , PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next(); // Proceed to the next middleware/route handler
+};
+
+// Use CORS middleware for all routes in this router
+router.use(cors);
+
 router.route("/").get(userController.getAllUsers).post(authController.register);
 router
   .route("/:id")
