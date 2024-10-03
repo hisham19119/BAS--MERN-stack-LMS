78425e395883556834c322d6eef429231862fedd
roma0000 <hishamtaha047@gmail.com>
2024-10-04 00:38:45 +0300

    added vercel.jsonn updated04

--- server/index.js
+++ server/index.js
@@ -9,16 +9,33 @@ const chapterRouter = require("./routes/chapters.routes");
 const categoryRouter = require("./routes/categories.routes");
 const path = require("path");
 const bodyParser = require("body-parser");
+const router = express.Router();
 
-const allowedOrigins = [
-  "https://lms-mern-stack-client-gif3m4zun-hisham19119s-projects.vercel.app",
-];
+const cors = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE , PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next(); // Proceed to the next middleware/route handler
+};
+
+// Use CORS middleware for all routes in this router
+router.use(cors);
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin:
+      "https://lms-mern-stack-client-gif3m4zun-hisham19119s-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
+    allowedHeaders: ["Content-Type"],
   })
 );
 
